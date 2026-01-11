5f8960f7c21cdc7876d26c0e86b55db2775471a2
Mayuri-Faye <mayu.wandhare@gmail.com>
2026-01-11 12:44:47 +0530

    mayu changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+# ----------mayuri-------------
+# ----------rudhu--------------
 
 @app.route("/", methods=["GET", "POST"])
 def login():
